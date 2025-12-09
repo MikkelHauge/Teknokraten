53678753b61d43f918553f58ab607a1f9d141602
Mikkel Hauge <mikkelhauge84+github@gmail.com>
2025-12-09 20:42:56 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Teknokraten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.UrlBox = new System.Windows.Forms.TextBox();
+            this.DownloadBtnMp3 = new System.Windows.Forms.Button();
+            this.DownloadBtnVideo = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UrlBox
+            // 
+            this.UrlBox.Location = new System.Drawing.Point(13, 13);
+            this.UrlBox.Name = "UrlBox";
+            this.UrlBox.Size = new System.Drawing.Size(259, 20);
+            this.UrlBox.TabIndex = 0;
+            // 
+            // DownloadBtnMp3
+            // 
+            this.DownloadBtnMp3.Location = new System.Drawing.Point(13, 39);
+            this.DownloadBtnMp3.Name = "DownloadBtnMp3";
+            this.DownloadBtnMp3.Size = new System.Drawing.Size(115, 23);
+            this.DownloadBtnMp3.TabIndex = 1;
+            this.DownloadBtnMp3.Text = "Download MP3";
+            this.DownloadBtnMp3.UseVisualStyleBackColor = true;
+            // 
+            // DownloadBtnVideo
+            // 
+            this.DownloadBtnVideo.Location = new System.Drawing.Point(151, 39);
+            this.DownloadBtnVideo.Name = "DownloadBtnVideo";
+            this.DownloadBtnVideo.Size = new System.Drawing.Size(121, 23);
+            this.DownloadBtnVideo.TabIndex = 2;
+            this.DownloadBtnVideo.Text = "Download Video";
+            this.DownloadBtnVideo.UseVisualStyleBackColor = true;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(12, 84);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(35, 13);
+            this.ResultLabel.TabIndex = 3;
+            this.ResultLabel.Text = "label1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 133);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(260, 116);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.DownloadBtnVideo);
+            this.Controls.Add(this.DownloadBtnMp3);
+            this.Controls.Add(this.UrlBox);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            DownloadBtnMp3.Click += DownloadBtnMp3_Click;
+            DownloadBtnVideo.Click += DownloadBtnVideo_Click;
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UrlBox;
+        private System.Windows.Forms.Button DownloadBtnMp3;
+        private System.Windows.Forms.Button DownloadBtnVideo;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Teknokraten
+{
+    public partial class Form1 : Form
+    {
+        private Process _runningProcess;
+        private readonly string ytDlpPath;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckYtDlpVersion();
+            DownloadBtnMp3.Click += DownloadBtnMp3_Click;
+            DownloadBtnVideo.Click += DownloadBtnVideo_Click;
+            this.Text = "Teknokraten";
+            ytDlpPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "yt-dlp", "yt-dlp.exe");
+            try
+            {
+                this.Icon = new System.Drawing.Icon("Resources\\teknokraten.ico");
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private async void DownloadBtnMp3_Click(object sender, EventArgs e)
+        {
+            string url = UrlBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                updateTextBox("Please enter a valid URL.");
+                return;
+            }
+
+            SetUIForDownloading();
+            try
+            {
+                await Task.Run(() => RunYtDlp($"-x --audio-format mp3 \"{url}\""));
+                updateTextBox("MP3 Download Complete.");
+            }
+            catch (Exception ex)
+            {
+                updateTextBox("Error: " + ex.Message);
+            }
+            finally
+            {
+                ResetUI();
+            }
+        }
+
+        private async void DownloadBtnVideo_Click(object sender, EventArgs e)
+        {
+            string url = UrlBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                updateTextBox("Please enter a valid URL.");
+                return;
+            }
+
+            SetUIForDownloading();
+            try
+            {
+                await Task.Run(() => RunYtDlp($"-f best \"{url}\""));
+                updateTextBox("Video Download Complete.");
+            }
+            catch (Exception ex)
+            {
+                updateTextBox("Error: " + ex.Message);
+            }
+            finally
+            {
+                ResetUI();
+            }
+        }
+
+        private void SetUIForDownloading()
+        {
+            this.Invoke((Action)(() =>
+            {
+                DownloadBtnMp3.Enabled = false;
+                DownloadBtnVideo.Enabled = false;
+                textBox1.Clear();
+            }));
+        }
+
+        private void ResetUI()
+        {
+            this.Invoke((Action)(() =>
+            {
+                DownloadBtnMp3.Enabled = true;
+                DownloadBtnVideo.Enabled = true;
+            }));
+        }
+
+        private void RunYtDlp(string arguments)
+        {
+            var process = new Process();
+            try
+            {
+                process.StartInfo.FileName = ytDlpPath;  // Use the correct path here
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.OutputDataReceived += (s, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        updateTextBox(e.Data);
+                };
+
+                process.ErrorDataReceived += (s, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        updateTextBox(e.Data);
+                };
+
+                process.Start();
+
+                _runningProcess = process;
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+                _runningProcess = null;
+            }
+            finally
+            {
+                process.Dispose();
+            }
+        }
+
+        private async void CheckYtDlpVersion()
+        {
+            ResultLabel.Text = "Looking for yt-dlp at: " + ytDlpPath;
+            string version = await Task.Run(() => RunProcessForOutput(ytDlpPath, "--version")); // Use path here too
+            ResultLabel.Text = version ?? "yt-dlp not found, in or failed to run.";
+        }
+
+        private void updateTextBox(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return;
+
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action(() =>
+                {
+                    textBox1.AppendText(text + Environment.NewLine);
+                }));
+            }
+            else
+            {
+                textBox1.AppendText(text + Environment.NewLine);
+            }
+        }
+
+        private string RunProcessForOutput(string fileName, string arguments)
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.FileName = fileName;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+
+                return output.Trim();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
